b627963648068c312aec462023edf4165c759306
Jeremiah Lumontod <jeremiah.lumontod@gmail.com>
2017-08-10 05:44:31 +0800

    added apiKey and authDomain from firebase websetup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,8 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     firebase.initializeApp({
-      apiKey: "AIzaSyDGDTtGNC4FkKpSe85qb2wLHzNi2xkxFy8",
-      authDomain: "ng-recipe-book.firebaseapp.com"
+      apiKey: "AIzaSyDaKqdvw6-5ylsNq4OHTt-UV6s3K375AqY",
+      authDomain: "ng-recipe-book-f9d31.firebaseapp.com"
     });
   }
 
